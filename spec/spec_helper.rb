245ab4930151dc3f85d74694c3b4bd049e044ae8
Todd Lunter <tlunter@gmail.com>
2013-08-04 17:28:10 -0400

    Clean up posts, and create tests for posts.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'rack/test'
+require 'factory_girl'
+
+FactoryGirl.find_definitions
 
 # setup test environment
 set :environment, :test
@@ -14,6 +17,7 @@ end
 set :views, Proc.new { File.join(App.root, 'spec', 'views') }
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.include Rack::Test::Methods
 end
 
