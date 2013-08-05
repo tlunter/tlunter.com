6813fd6e72faa95f22bfb1e556a920be303ab645
Todd Lunter <tlunter@gmail.com>
2013-08-04 20:27:59 -0400

    Must turn off show_exceptions!

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ require 'pry'
 class App < Sinatra::Application
   set :protection, :origin_whitelist => ['http://localhost']
   set :views, File.join(App.root, 'app', 'views')
+  set :show_exceptions, true unless ENV['TLUNTER_ENV'] == 'production'
 
   before %r{.*\.json$} do
     content_type :json
