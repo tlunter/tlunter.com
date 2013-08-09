3270d4b2814ff8b9dc68978c640ac2877a3a9462
Todd Lunter <tlunter@gmail.com>
2013-08-08 22:38:35 -0400

    Add comments#show spec and route

--- spec/routes/comments_spec.rb
+++ spec/routes/comments_spec.rb
@@ -6,21 +6,78 @@ describe 'comments routes' do
   let!(:comment1) { FactoryGirl.build(:comment, :post => new_post).tap { |c| c.save } }
   let!(:comment2) { FactoryGirl.build(:comment, :post => new_post).tap { |c| c.save } }
 
+  let!(:markdown_comments) do
+    [ comment1, comment2 ].each do |c|
+      c[:body] = App.markdown.render c[:body]
+    end
+  end
+
   context 'for a posts comment index' do
-    before do
-      get "/comments/#{new_post.link}.json"
+    context 'with a good post-link' do
+      before do
+        get "/comments/#{new_post.link}.json"
+      end
+
+      it "responds ok" do
+        expect(last_response).to be_ok
+      end
+
+      it "body should be the comments as json" do
+        expect(last_response.body).to be == markdown_comments.to_json
+      end
+
+      it "to have the json content type" do
+        expect(last_response.content_type).to include "application/json"
+      end
     end
 
-    it "responds ok" do
-      expect(last_response).to be_ok
+    context 'with a bad post-link' do
+      before do
+        get "/comments/a-known-bad-link.json"
+      end
+
+      it "responds not found" do
+        expect(last_response).to be_not_found
+      end
+    end
+  end
+
+  context 'for a posts comment show' do
+    context 'with a good post-link' do
+      context 'with a good comment-id' do
+        before do
+          get "/comments/#{new_post.link}/#{comment1.id}.json"
+        end
+
+        it "responds ok" do
+          expect(last_response).to be_ok
+        end
+
+        it "body should have the comment" do
+          expect(last_response.body).to be == markdown_comments.first.to_json
+        end
+      end
+
+      context 'with a bad comment-id' do
+        before do
+          get "/comments/#{new_post.link}/0.json"
+        end
+
+        it "responds not found" do
+          expect(last_response).to be_not_found
+        end
+      end
     end
 
-    it "body should be the comments as json" do
-      expect(last_response.body).to be == [comment1, comment2].map { |c| c[:body] = App.markdown.render c[:body]; c }.to_json
-    end
+    context 'with a bad post-link' do
+      before do
+        get "/comments/a-known-bad-link/#{comment1.id}.json"
+      end
 
-    it "to have the json content type" do
-      expect(last_response.content_type).to include "application/json"
+      it "responds not found" do
+        expect(last_response).to be_not_found
+      end
     end
   end
 end
+
