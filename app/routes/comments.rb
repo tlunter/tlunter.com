e7f0eaec624bad9db23ba2d53d0119260af77066
Todd Lunter <tlunter@gmail.com>
2013-08-08 22:38:35 -0400

    Add comments#show spec and route

--- app/routes/comments.rb
+++ app/routes/comments.rb
@@ -1,10 +1,24 @@
 get %r{/comments/([\w-]+)\.json} do |post_link|
   post = Post.first(:link => post_link)
+
+  halt 404 unless post
+
   comments = post.comments.all(:order => :created_at.asc)
   
-  comments.map do |comment|
-    c = comment.as_json
+  comments.each do |c|
     c[:body] = App.markdown.render c[:body]
-    c
   end.to_json
+end
+
+get %r{/comments/([\w-]+)/(\d+)\.json} do |post_link, comment_id|
+  post = Post.first(:link => post_link)
+
+  halt 404 unless post
+
+  comment = post.comments.first(:id => comment_id)
+
+  halt 404 unless comment
+
+  comment[:body] = App.markdown.render comment[:body]
+  comment.to_json
 end
