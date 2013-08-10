f3f6cb48ca4010aa2f72db155dbefd038432c353
Todd Lunter <tlunter@gmail.com>
2013-08-09 23:48:39 -0400

    Frontend now has comments
    Worked with the backend slightly and the frontend Angular code to drive
    the inline form for posting comments.

--- public/js/posts_module.js
+++ public/js/posts_module.js
@@ -2,42 +2,132 @@ var posts_module = angular.module('posts_module', [])
 
 posts_module.value('$anchorScroll', angular.noop);
 
+posts_module.factory('PostsFactory', ['$http', function ($http) {
+  return {
+    getData: function (post, callback) {
+      if (post === undefined) {
+        post = 'latest';
+      } else {
+        post = 'i/' + post;
+      }
+
+      post_route = '/post/' + post + '.json';
+
+      $http.get(post_route).
+        success(function (data, status, headers, config) {
+          $.each(data, setupPost);
+          callback(true, data);
+        }).
+        error(function (data, status, headers, config) {
+          callback(false, data);
+        });
+    }
+  }
+}]);
+
+posts_module.factory('CommentsFactory', ['$http', function ($http) {
+  return {
+    getData: function (post, callback) {
+      if (post === undefined) {
+        return;
+      }
+
+      comments_route = '/comments/' + post + '.json';
+
+      $http.get(comments_route).
+        success(function(data, status, headers, config) {
+          callback(true, data);
+        }).
+        error(function (data, status, headers, config) {
+          callback(false, data);
+        });
+    },
+    getCsrf: function (post, callback) {
+      if (post === undefined) {
+        return;
+      }
+
+      comments_route = '/comments/' + post + '/new.json';
+
+      $http.get(comments_route).
+        success(function(data, status, headers, config) {
+          callback(data['csrf_token']);
+        });
+    },
+    postComment: function(post, email, body, csrf, callback) {
+      if (post === undefined) {
+        return;
+      }
+
+      comments_route = '/comments/' + post + '/new.json';
+
+      $http.post(
+          comments_route,
+          $.param({ 'email': email, 'body': body, '_csrf': csrf }),
+          { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).
+        success(function(data, status, headers, config) {
+          callback(true, data);
+        }).
+        error(function (data, status, headers, config) {
+          callback(false, data);
+        });
+    }
+  }
+}]);
+
+posts_module.controller('PostsController',
+    ['$scope', '$routeParams', '$http', 'PostsFactory', 'CommentsFactory',
+    function ($scope, $routeParams, $http, PostsFactory, CommentsFactory) {
+
+  var post = $routeParams['id']
+  var csrf_token;
+
+  PostsFactory.getData(post, function(success, data) {
+    if (!success) {
+      $scope['current'] = null;
+      return;
+    }
+
+    $.extend($scope, data);
+
+    var link = data['current']['link'];
+
+    CommentsFactory.getData(link,
+      function(success, commentData) {
+      if (!success) {
+        $scope['comments'] = null;
+      }
+
+      $.extend($scope, commentData);
+    });
+
+    CommentsFactory.getCsrf(link, function (ct) {
+      csrf_token = ct;
+    });
+  });
+
+  $scope.postComment = function () {
+    CommentsFactory.postComment($scope.current['link'],
+      $scope.comment_email, $scope.comment_body, csrf_token,
+      function (success, data) {
+        console.log(data);
+      });
+  };
+}]);
+
 posts_module.config(function($locationProvider, $routeProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
   $routeProvider.
     when('/posts/latest', {
-      controller: PostsController,
+      controller: 'PostsController',
       templateUrl: '/partials/post.html'
     }).
     when('/posts/i/:id', {
-      controller: PostsController,
+      controller: 'PostsController',
       templateUrl: '/partials/post.html'
     }).
     otherwise({ redirectTo: '/posts/latest' });
 });
-
-function PostsController($scope, $route, $routeParams, $http) {
-  post = $routeParams['id']
-  if (post === undefined)
-    post = 'latest';
-  else
-    post = 'i/' + post;
-
-  $scope['previous'] = 'none';
-  $scope['next'] = 'none';
-
-  post_route = '/post/' + post + '.json';
-  $http.get(post_route).
-    success(function (data, status, headers, config) {
-      $.each(data, setupPost);
-      $scope['previous'] = data['previous'];
-      $scope['next'] = data['next'];
-      $scope['post'] = data['current'];
-    }).
-    error(function (data, status, headers, config) {
-      $scope['post'] = null;
-    });
-}
 
 function setupPost(key, value) {
   if (value !== null) {
