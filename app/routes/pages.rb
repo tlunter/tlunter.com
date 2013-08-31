b680d3c1790b0add83e0eb34443f8db2b2b8008b
Todd Lunter <tlunter@gmail.com>
2013-08-31 09:51:27 -0400

    Add an ip whitelist for certain pages

--- app/routes/pages.rb
+++ app/routes/pages.rb
@@ -1,5 +1,15 @@
 require 'json'
 
+IP_WHITELIST_PAGES = ['/posts/new']
+
+IP_WHITELIST_PAGES.each do |page|
+  before page do
+    unless request.env['HTTP_X_REAL_IP'] == '127.0.0.1' || request.env['HTTP_X_REAL_IP'] =~ /192\.168\.1\.\d{1,3}/
+      redirect '/'
+    end
+  end
+end
+
 before '*' do
   unless request.path_info.end_with?(".json")
     request.path_info = '/'
