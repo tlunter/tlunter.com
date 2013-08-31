5a31f2d3f4e5e7ac41bb49bd7dbaaf6699e63ee6
Todd Lunter <tlunter@gmail.com>
2013-08-31 18:57:11 -0400

    Fix feeds

--- app/assets/js/feeds/feeds.js
+++ app/assets/js/feeds/feeds.js
@@ -1,23 +1,29 @@
 angular.module('feeds', ['resource.feed', 'services.date']);
 
 angular.module('feeds').controller('FeedsController',
-    ['$scope', '$route', '$routeParams', '$http', 'Feed', 'DateFormatter',
-    function($scope, $route, $routeParams, $http, Feed, DateFormatter) {
-  feed = $routeParams['feed'];
-
-  var data = Feed.get({ feed: feed }, function () {
-    angular.forEach(data, function(item) {
-      item = DateFormatter.setupDate(item, 'published_date');
-    });
-    $scope[feed] = 'active';
-    $scope['feed'] = data;
-  });
+    ['$scope', 'feedItems', 'feed',
+    function($scope, feedItems, feed) {
+  $scope.feedItems = feedItems;
+  $scope.feed = feed;
 }]);
 
 angular.module('feeds').config(
   ['$routeProvider', function($routeProvider) {
   $routeProvider.when('/about/feed/:feed', {
     templateUrl: '/partials/feed.html',
-    controller: 'FeedsController'
+    controller: 'FeedsController',
+    resolve: {
+      feedItems: ['$route', 'Feed', 'DateFormatter', function ($route, Feed, DateFormatter) {
+        var data = Feed.get({ feed: $route.current.params.feed }, function () {
+          angular.forEach(data, function(item) {
+            item = DateFormatter.setupDate(item, 'published');
+          });
+        });
+        return data;
+      }],
+      feed: ['$route', function ($route) {
+        return $route.current.params.feed;
+      }]
+    }
   });
 }]);
