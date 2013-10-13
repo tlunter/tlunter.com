82086511f186542fca7e941059f365a9f56c158f
Todd Lunter <tlunter@gmail.com>
2013-10-13 08:54:01 -0400

    Change database connection style

--- config/database.rb
+++ config/database.rb
@@ -1,3 +1,16 @@
 require 'data_mapper'
 
-DataMapper.setup(:default, 'mysql://tluntercom:@localhost/tluntercom')
+DATABASE_ADAPTER  = 'mysql'
+DATABASE_USER     = 'tluntercom'
+DATABASE_PASSWORD = ''
+DATABASE_HOST     = 'localhost'
+DATABASE_DB       = 'tluntercom'
+DATABASE_TEST     = 'test_tluntercom'
+
+DataMapper.setup(:default, {
+  adapter: DATABASE_ADAPTER,
+  username: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  database: DATABASE_DB
+})
