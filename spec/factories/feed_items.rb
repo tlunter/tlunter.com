cd55d9937ad1262b05521d3cfd02d168845a04ca
Todd Lunter <tlunter@gmail.com>
2014-06-25 15:51:36 -0400

    Remove old tests

--- spec/factories/feed_items.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :feed_item do
-    sequence(:title) { |n| "title-#{n}" }
-    sequence(:link) { |n| "link-#{n}" }
-    published { DateTime.now }
-    type GitHubItem
-  end
-end
